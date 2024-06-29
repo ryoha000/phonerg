9678c6de9c2205c056039e5e5ea5c1f84f196009
ryoha000 <the7uya@gmail.com>
2024-06-30 05:18:43 +0900

    feat: 🎸 initial commit

---
+++ app/src/main/java/moe/ryoha/phonerg/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package moe.ryoha.phonerg.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AccentAccent = Color(0xFF487AF9)
+val AccentSuccess = Color(0xFF347d39)
+val AccentWarning = Color(0xFFc69026)
+val AccentError = Color(0xFFEA4E60)
+
+val BgPrimary = Color(0xFF22272e)
+val BgSecondary = Color(0xFF2d333b)
+val BgTertiary = Color(0xFF323942)
+val BgDisabled = Color(0xFF181818)
+val BgButton = Color(0xFF373e47)
+
+val BorderPrimary = Color(0xFF444c56)
+val BorderButton = Color(0xFFCDD9E5)
+
+val TextPrimary = Color(0xFFadbac7)
+val TextSecondary = Color(0xFFCDD9E5)
+val TextTertiary = Color(0xFF768390)
+val TextLink = Color(0xFF2e7cd5)
+val TextDisabled = Color(0xFF484f58)
